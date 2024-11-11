5bf4d6adb144e4e9b53cccf88153415a9f30eb5a
ALEX <segunfawaz2019@gmail.com>
2024-11-10 16:51:24 -0800

    part2

--- src/contexts/BalanceContext.js
+++ src/contexts/BalanceContext.js
@@ -5,17 +5,15 @@ const BalanceContext = createContext();
 export const BalanceProvider = ({ children }) => {
   const [balance, setBalance] = useState(() => {
     const savedBalance = localStorage.getItem('balance');
-    return savedBalance ? parseFloat(savedBalance) : 0; // Ensure it's 0 if there's no saved balance
+    return savedBalance ? parseFloat(savedBalance) : 0;
   });
 
-  // This will update balance in both state and localStorage
+  useEffect(() => {
+    localStorage.setItem('balance', balance);
+  }, [balance]);
+
   const addToBalance = (amount) => {
-    setBalance((prevBalance) => {
-      const newBalance = prevBalance + amount;
-      // Store the updated balance in localStorage immediately
-      localStorage.setItem('balance', newBalance.toFixed(3));
-      return newBalance;
-    });
+    setBalance((prevBalance) => prevBalance + amount);
   };
 
   return (
